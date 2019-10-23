069f94a8e9aac79cb066277492a782d3a07ed704
Ulysse Rubens <ulyrubens@hotmail.com>
2019-10-23 13:48:14 +0200

    Update doc links

--- src/components/navbar/AboutCytomineModal.vue
+++ src/components/navbar/AboutCytomineModal.vue
@@ -51,8 +51,7 @@
       <dt>{{$t('documentation')}}</dt>
       <dd>
         <i18n path="documentation-info">
-          <a place="docLink" href="https://neubias-wg5.github.io" target="_blank">{{$t('here')}}</a>
-          <a place="apiDocLink" :href="apiDocLink" target="_blank">{{$t('here')}}</a>
+          <a place="docLink" href="https://neubias-wg5.github.io/user_guide.html" target="_blank">{{$t('here')}}</a>
         </i18n>
       </dd>
 
